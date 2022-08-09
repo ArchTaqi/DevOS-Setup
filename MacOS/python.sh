a3c8e06d7dc710999f3d6d2960fc34e43ff4ba07
Muhαmmαd Tαqi <taqi.arch@gmail.com>
2022-08-10 01:21:03 +0500

    Update python.sh

--- MacOS/python.sh
+++ MacOS/python.sh
@@ -38,3 +38,31 @@ pip install Flask
 pip install sqlalchemy
 #pip install mysql-python  # Python 2 only, use mysqlclient instead
 pip install mysqlclient
+
+pip3 install astroid
+pip3 install autopep8
+pip3 install certifi
+pip3 install chardet2
+pip3 install click
+pip3 install Flask
+pip3 install Flask-Cors
+pip3 install harperdb
+pip3 install idna
+pip3 install isort
+pip3 install itsdangerous
+pip3 install Jinja
+pip3 install lazy-object-proxy
+pip3 install MarkupSafe
+pip3 install mccabe
+pip3 install psycopg2
+pip3 install psycopg2-binary
+pip3 install pycodestyle
+pip3 install pylint
+pip3 install python-dotenv
+pip3 install requests
+pip3 install setuptools
+pip3 install six
+pip3 install toml
+pip3 install urllib3
+pip3 install Werkzeug
+pip3 install wrapt
