f806f8a0b94e0f8f682643fc242ecdedb4e2219e
Muhαmmαd Tαqi <taqi.arch@gmail.com>
2022-08-10 01:20:39 +0500

    Update node.sh

--- MacOS/node.sh
+++ MacOS/node.sh
@@ -4,3 +4,25 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install --lts
 nvm use --lts
 
+npm i -g @aws-amplify/cli
+npm i -g @sanity/cli
+npm i -g babel-cli
+npm i -g eslint
+npm i -g expo-cli
+npm i -g firebase-tools
+npm i -g gatsby-cli
+npm i -g jest
+npm i -g lighthouse
+npm i -g netlify-cli
+npm i -g newman
+npm i -g node-sass
+npm i -g parcel-bundler
+npm i -g pm2
+npm i -g prettier
+npm i -g serve
+npm i -g spaceship-prompt
+npm i -g surge
+npm i -g typescript
+npm i -g update
+npm i -g vercel
+npm i -g yarn
