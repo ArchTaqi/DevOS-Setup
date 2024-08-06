4e9b55a9a3d2a78aa991542c8a3edb8d7d869b90
Muhammad Taqi <taqi.arch@gmail.com>
2024-08-06 18:56:58 +0500

    Update node.sh

--- MacOS/node.sh
+++ MacOS/node.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 nvm install --lts
 nvm use --lts
 
