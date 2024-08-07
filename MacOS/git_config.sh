7c7fa61f88d1bc50005d3240d87f020442f7bb77
Muhammad Taqi <taqi.arch@gmail.com>
2024-08-07 11:59:09 +0500

    Create git_config.sh

---
+++ MacOS/git_config.sh
@@ -0,0 +1,8 @@
+git config --global user.name "archtaqi"
+git config --global user.email "taqi.arch@gmail.com"
+
+git config --global core.editor nvim
+git config --global core.excludesfile ~/.gitignore
+
+echo "New Git Config"
+git config --list
