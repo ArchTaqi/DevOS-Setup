d8024d8fb8f0738df9e42b1cdbf605e91a973f97
Muhammad Taqi <taqi.arch@gmail.com>
2024-08-25 01:39:18 +0500

    Delete MacOS/git_config.sh

--- MacOS/git_config.sh
+++
@@ -1,8 +0,0 @@
-git config --global user.name "archtaqi"
-git config --global user.email "taqi.arch@gmail.com"
-
-git config --global core.editor nvim
-git config --global core.excludesfile ~/.gitignore
-
-echo "New Git Config"
-git config --list
